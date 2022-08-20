609ea459fde9032c9e3b448febd6f61d2e4656fa
Massivebutterpapper <106040279+Massivebutterpapper@users.noreply.github.com>
2022-08-20 11:09:15 -0700

    Clever
    Clever Systems

---
+++ Clever 1.0.0.bat
@@ -0,0 +1,94 @@
+@echo off 
+cd /
+cls
+color 17
+echo Starting Clever...
+echo Failed to start
+echo Please report this at RavenSystems
+echo please press f8 to try again
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo Connecting....
+echo Keyboard detected
+echo Mouse detected
+echo System ready please press f11
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                            CLEVER
+echo
+echo                                                                       PROGRAMED EDITION
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                                                                           FAILED TO START
+echo
+ 
+
+echo                                                              THIS VERSION OF CLEVER HAS ENDED SUPPORT
+
+
+
+
+
+
+
+
+
+
+
+
+
+echo                                                                   SHUTING OFF...
+
+
+
+
+
+
+
+
+pause
+ 
